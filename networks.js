39bf159a955363b086e2d9b70782142ca0a10b88
Daniel Luca <lucadanielcostin@gmail.com>
2020-04-19 12:46:38 +0200

    Feature/bet details bettors and options (#8)
    * Return bettors and selected options.
    
    * Add infura network.
    
    * Fix lint errors.

--- networks.js
+++ networks.js
@@ -12,5 +12,12 @@ module.exports = {
             networkId: 5777,
             gasPrice: 1,
         },
+        ropsten: {
+            provider: () => new HDWalletProvider(
+                mnemonic, 'https://ropsten.infura.io/v3/aae9ac7ee0e14e4ea9d7cced4bcee1a5',
+            ),
+            networkId: 3,
+            gasPrice: 1000,
+        },
     },
 }

8b874176a5807908ef05b2e0391eec6c84e255a3
Daniel Luca <lucadanielcostin@gmail.com>
2020-04-26 18:15:14 +0300

    Create migration for truffle. Add ganache network.

---
+++ migrations/1587913901_trustbet.js
@@ -0,0 +1,6 @@
+var TrustBet = artifacts.require("TrustBet")
+
+module.exports = function (_deployer) {
+    _deployer.deploy(TrustBet);
+
+};
--- truffle-config.js
+++ truffle-config.js
@@ -26,12 +26,12 @@
 
 module.exports = {
     networks: {
-        // development: {
-        //     host: '127.0.0.1',
-        //     port: 8545,
-        //     network_id: '1337',
-        //     websockets: true,
-        // },
+        ganache: {
+            host: '127.0.0.1',
+            port: 7545,
+            network_id: '5777',
+            websockets: true,
+        },
     },
 
     plugins: ['solidity-coverage'],
